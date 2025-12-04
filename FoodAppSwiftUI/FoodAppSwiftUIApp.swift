4b864aad78461976464d46cff2331a0d5d20baf8
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2025-12-04 14:23:04 +0400

    Initial Commit

---
+++ FoodAppSwiftUI/FoodAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppSwiftUIApp.swift
+//  FoodAppSwiftUI
+//
+//  Created by Hakob Ghlijyan on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
